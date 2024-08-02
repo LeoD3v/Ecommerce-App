7ac8137df047e42c6be115dad3e783dab9ef422b
Anastasiia <anastasiaaaguseva@gmail.com>
2024-08-02 16:42:45 +0200

    routes

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const Order = require("../models/Order.model"); 
 const Item = require("../models/Item.model"); 
-const Inventory = require("../models/Inventory.model"); 
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
@@ -41,16 +40,6 @@ router.post('/item/create', async (req, res) => {
     const item = new Item(req.body);
     await item.save();
     res.status(201).send(item);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-router.post('/inventory/create', async (req, res) => {
-  try {
-    const inventory = new Inventory(req.body);
-    await inventory.save();
-    res.status(201).send(inventory);
   } catch (error) {
     res.status(400).send(error);
   }
